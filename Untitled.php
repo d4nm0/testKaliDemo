b4aaa5c76e60b7eedf216435d735b164fefba141
Dan Monceau <dan@eisge.com>
2023-06-02 15:45:47 +0200

    S 9 -

--- Untitled.php
+++ Untitled.php
@@ -39,7 +39,7 @@ class User extends BaseApi
     function AddImageProfil()
     {
         $d = $this->checkParams(
-            [fdsdfsdfsddkjkhshjsfng'
+            [fdsdfsdfsddkgdfdjkhshjsfng'
             ]
         );
 
