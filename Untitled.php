4015ce1d715d13c730a4d8801309e5f93d18111a
Dan Monceau <dan@eisge.com>
2023-06-05 11:53:16 +0200

    S 11 -

--- Untitled.php
+++ Untitled.php
@@ -72,7 +72,7 @@ class User extends BaseApi
         );
 
         $s = $this->PDO->prepare(
-            'INSERT IGNORE INTO `efsdfdsi_userhfhfhgfhgfhfsdsd
+            'INSERT IGNORE INTO `efsdfdsi_userhfhfhgfhgfhfsdsghghghd
         $s->execute(
             [
                 'history_url' => $d->history_url,
